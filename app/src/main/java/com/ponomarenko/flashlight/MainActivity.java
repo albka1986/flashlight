51a5e5e0eca233858a5c13a5e2aeb84e2f22633a
Ponomarenko Oleh <albka1986@gmail.com>
2017-08-27 22:57:02 +0300

    implemented active bar menu (without the method "Share the App");

--- app/src/main/java/com/ponomarenko/flashlight/MainActivity.java
+++ app/src/main/java/com/ponomarenko/flashlight/MainActivity.java
@@ -1,20 +1,27 @@
 package com.ponomarenko.flashlight;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
+import android.net.Uri;
 import android.os.Build;
 import android.hardware.Camera;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MAIL_REQUEST = 1110;
     private Camera camera;
     private CameraManager camManager;
     private String cameraId;
@@ -28,6 +35,44 @@ public class MainActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, 1);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_share_app:
+                shareApp();
+                break;
+
+            case R.id.meu_item_contact_us:
+                startEmailClient();
+                break;
+        }
+        return true;
+    }
+
+    private void startEmailClient() {
+        Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:" + TextUtils.join(",", new String[]{getString(R.string.developer_email)})));
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject));
+        startActivityForResult(Intent.createChooser(intent, "Invite friends"), MAIL_REQUEST);
+
+    }
+
+    private void shareApp() {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+
+        intent.setType("text/plain");
+        //TODO: past the link on the App in  Google Play  and set Enable for this item menu;
+//        intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.url_google_play));
+        startActivity(Intent.createChooser(intent, "Share"));
     }
 
     @Override
